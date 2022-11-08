d0ca06c37e87f2008dd46e0aa1c5b1518fe69cd5
Jerusalemgirma <jerusalemtesfaye00@gmail.com>
2022-11-08 11:36:29 +0300

    i

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,19 +1,44 @@
-class solution:
-    def countSwaps(a):
-       
-        sorted = False
-        swaps = 0
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps(a):
+    # Write your code here
+    count = 0
+    for i in range(len(a)):
+        for j in range(len(a)- 1):
+            if (a[j] > a[j + 1]):
+                s = a[j]
+                a[j] = a[j+1]
+                a[j + 1] = s
+                count += 1
         
-        while not sorted:
-            sorted = True
-            
-            for i in range(len(a)-1):
-                if a[i] > a[i +1]:
-                    a[i], a[i+1] = a[i+1], a[i]
-                    swaps +=1
-                    sorted = False
-        
-        print("Array is sorted in",swaps, "swaps.")
-        print("First Element:", a[0])
-        print("Last Element:", a[len(a)-1])
-        return a
+        a, swapcount = 0, 0
+    while True:
+        swapped = False
+        for i in range(1, len(a)):
+            a += 1
+            if a[i-1] > a[i]:
+                swapcount += 1
+                a[i-1], a[i] = a[i], a[i-1]
+                swapped = True
+        if not swapped:
+            break
+    return a, swapcount
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
